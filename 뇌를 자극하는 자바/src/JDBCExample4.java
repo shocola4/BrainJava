fb541199cbef30b0dec078d2949f0b5470d6e412
Administrator <Administrator@KGH>
2018-05-08 14:19:48 +0900

    0508

---
+++ 뇌를 자극하는 자바/src/JDBCExample4.java
@@ -0,0 +1,55 @@
+
+//패키지
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql. ResultSet;
+
+class JDBCExample4 {
+    public static void main(String args[]) {
+    	//1단계:DB 연결을 위한 커넥션 인터페이스
+        Connection conn = null;
+        // Statement 인터페이스-SQL 실행
+        Statement  stmt = null;
+        // ResultSet 인터페이스-SQL 결과를 저장
+        ResultSet rs = null;
+        //try ~catch문에서 DB연결중에 예외가 발생하느지를 검사.
+        try {
+        	//2단계;JDB드라이버를 로드한다.
+            Class.forName("com.mysql.jdbc.Driver");
+            //3단계:드라이버매니져 클래스는 DB를 연결한다.
+            conn = DriverManager.getConnection(
+               "jdbc:mysql://localhost:3307/mysql", "root", "12345");
+            System.out.println("데이터베이스에 접속했습니다.");
+            
+            //커넥션 객체가 Statement객체를 생성
+            stmt=conn.createStatement();
+            //DML SQL쿼리 실행후 결과를 저장
+            rs=stmt.executeQuery(" select goodsinfocode ,	goodsinfoname,	price,	maker from goodsinfo");		
+            
+            System .out. println(" 상품코드 상품명 가격 제조사");
+            
+            while(rs.next()) {
+            
+            String goodsinfocode = rs.getString(1);//rs.getString(jumincd);
+            String goodsinfoname= rs .getString(2);//getString("pname");
+            String price= rs .getString(3);//getString("gender");
+            String maker= rs .getString(4);//getString("gender");
+          
+            
+            System .out. println(goodsinfocode+"---"+goodsinfoname+"--"+price+"---"+maker);
+            }
+            
+            //4단계:DB연결을 종료한다.
+            conn.close();
+        }
+        catch (ClassNotFoundException cnfe) {
+            System.out.println("해당 클래스를 찾을 수 없습니다." + 
+                               cnfe.getMessage());
+        }
+        catch (SQLException se) {
+            System.out.println(se.getMessage());
+        }
+    }
+}
