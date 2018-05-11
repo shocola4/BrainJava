06188649402d2e9754acef3023c549bf8edccffa
Administrator <Administrator@KGH>
2018-05-11 14:17:33 +0900

    0511-geometry

---
+++ 뇌를 자극하는 자바/src/geometry/Circle.java
@@ -0,0 +1,15 @@
+package geometry;
+
+public class Circle {
+	
+	int radius;
+	
+	public Circle(int radius) { 
+		this. radius = radius;
+		}
+	double getArea() {
+		return radius*radius*3.14;
+	}
+
+	
+}
---
+++ 뇌를 자극하는 자바/src/geometry/PackExample1.java
@@ -0,0 +1,19 @@
+/**
+ *2018. 5. 11. Dev By Kim HYUNAH
+  geometry
+  PackExample1.java
+ */
+package geometry;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PackExample1 {
+
+	public static void main(String[] args) {
+		Circle circle= new Circle (5);
+		System.out.println("반지름="+circle.radius);
+		
+	}
+}
