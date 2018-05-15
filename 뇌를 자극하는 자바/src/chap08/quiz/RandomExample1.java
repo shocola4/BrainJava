c1a5f613966c64a7984a7f29bc0b333161b6a539
Administrator <Administrator@KGH>
2018-05-15 10:06:48 +0900

    0515-난수 발생 프로그램

---
+++ 뇌를 자극하는 자바/src/chap08/quiz/RandomExample1.java
@@ -0,0 +1,16 @@
+package chap08.quiz;
+
+import java.util.Random;
+
+public class RandomExample1 {
+
+	public static void main(String[] args) {
+		Random random= new Random();
+		System.out.println(random.nextInt(100));
+		System.out.println(random.nextInt(100));
+		System.out.println(random.nextInt(100));
+
+		
+	}
+
+}
