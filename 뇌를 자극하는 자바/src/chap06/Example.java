c1428fe745bac61e42d10094047f4438436ea21b
Administrator <Administrator@KGH>
2018-05-16 16:08:34 +0900

    0516-두수교환

--- 뇌를 자극하는 자바/src/chap06/Example.java
+++ 뇌를 자극하는 자바/src/chap06/Example.java
@@ -1,25 +1,24 @@
 package chap06;
 
 public class Example {
-
-	public static void main(String[] args) {
-		String str="Information!";
-		int n=str.length();
-		char[]st=new char[n];
-		n--;
-		for(int k =n; k>=0; k--) {
-			st[n-k]=str.charAt(k);
-		}
-		for(char k :st) {
-			System.out.printf("%c",k);
-		}
-  /*  int[]a= {90,100,80,70,60,50,30};
-    int hap=0;
-    float avg;
-    for(int i:a)
-    	hap=hap+i;
-    avg=(float)hap/a.length;
-    System.out.printf("%4d %4.2f",hap,avg);*/
+	static class AAclass{
+		int i;
+		int j;
 	}
 
+	public static void main(String[] args) {
+		AAclass myVal=new AAclass();
+		myVal.i=10;
+		myVal.j=20;
+		myVal=change(myVal);
+		System.out.printf("i=%d,j=%d\n", myVal.i,myVal.j);
+	}
+	static AAclass change(AAclass myVal) {
+		int temp;
+		temp=myVal.i;
+		myVal.i=myVal.j;
+		myVal.j=temp;
+	
+		return myVal;
+	}
 }
