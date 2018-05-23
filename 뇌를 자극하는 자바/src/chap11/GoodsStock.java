2a39ae617abcbb7eabbf9eb13d837580f6c404f2
Administrator <Administrator@KGH>
2018-05-23 17:54:46 +0900

    0523-재고확인

---
+++ 뇌를 자극하는 자바/src/chap11/GoodsStock.java
@@ -0,0 +1,21 @@
+package chap11;
+
+public class GoodsStock {
+	String goodsCode;
+	int stockNum;
+	GoodsStock(String goodsCode,int stockNum){
+		this.goodsCode=goodsCode;
+		this.stockNum=stockNum;
+	}
+	@Override
+	public String toString() {
+		
+		String str ="상품코드:"+goodsCode+
+				    "재고수량:"+stockNum;
+
+		return str;
+	}
+	
+	
+
+}
---
+++ 뇌를 자극하는 자바/src/chap11/ObjectExample1.java
@@ -0,0 +1,13 @@
+package chap11;
+
+import java.io.File;
+
+public class ObjectExample1 {
+	
+	public static void main(String args[]) {
+		File file =new File("C:\\뻐구기");
+		String str=file.toString();
+		System.out.println(file);
+	}
+
+}
