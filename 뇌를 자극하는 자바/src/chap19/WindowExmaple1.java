2b2580997ec5e8286e85659c5f35df57ab96aa57
Administrator <Administrator@KGH>
2018-05-24 16:04:26 +0900

    0524-GUI

--- 뇌를 자극하는 자바/src/chap19/WindowExmaple1.java
+++ 뇌를 자극하는 자바/src/chap19/WindowExmaple1.java
@@ -18,7 +18,7 @@ public class WindowExmaple1 {
 		
 		frame.setPreferredSize(new Dimension(300,200));
 		Container contentPane = frame.getContentPane();
-		JLabel label=new JLabel("Hello, Java",SwingConstants.CENTER);
+		JLabel label=new JLabel("こんにちは, ザーバ",SwingConstants.CENTER);
 		contentPane.add(label);
 		
 		frame.pack();
