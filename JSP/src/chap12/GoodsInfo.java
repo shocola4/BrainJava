ef010c2d285a8ed2b1cbe24d97dfdc36272d7cb7
sec <sec@sec-PC>
2018-08-03 16:33:03 +0900

    상품,회원정보

---
+++ JSP/src/chap12/GoodsInfo.java
@@ -0,0 +1,42 @@
+package chap12;
+/* gcode CHAR(5),
+title VARCHAR(50),
+writer VARCHAR(20),
+price int(8),*/
+
+public class GoodsInfo {
+
+
+	String gcode;
+	String title;
+	String writer;
+	int price;
+	
+	public String getGcode() {
+		return gcode;
+	}
+	public void setGcode(String gcode) {
+		this.gcode = gcode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
---
+++ JSP/src/chap12/Userinfo.java
@@ -0,0 +1,32 @@
+package chap12;
+//uname     VARCHAR(50),
+//uid            VARCHAR(50),
+//upassword VARCHAR(50),
+//PRIMARY KEY(uid)
+
+public class Userinfo {
+  
+	String uname;
+	String uid;
+	String upassword;
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpassword() {
+		return upassword;
+	}
+	public void setUpassword(String upassword) {
+		this.upassword = upassword;
+	}
+	
+	
+}
